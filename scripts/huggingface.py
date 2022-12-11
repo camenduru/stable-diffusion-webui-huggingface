0e6cbe0a108fbe8fbd4350e9d1579b87cf22cf5a
camenduru <54370274+camenduru@users.noreply.github.com>
2022-12-11 13:08:26 +0300

    Update huggingface.py

--- scripts/huggingface.py
+++ scripts/huggingface.py
@@ -1,55 +1,100 @@
 import os
 import numpy as np
 import gradio as gr
-from huggingface_hub import model_info, create_repo, create_branch, upload_folder
+from huggingface_hub import model_info, create_repo, create_branch, upload_folder, upload_file
 from huggingface_hub.utils import RepositoryNotFoundError, RevisionNotFoundError
 from modules import scripts, script_callbacks
 
-def push_ckpt(model_from, model_to, token, branch):
+def push_folder(folder_from, folder_to, branch, token):
     try:
         repo_exists = True
-        r_info = model_info(model_to, token=token)
+        r_info = model_info(folder_to, token=token)
     except RepositoryNotFoundError:
         repo_exists = False
     finally:
         if repo_exists:
             print(r_info)
         else:
-            create_repo(model_to, private=True, token=token)
+            create_repo(folder_to, private=True, token=token)
     try:
         branch_exists = True
-        b_info = model_info(model_to, revision=branch, token=token)
+        b_info = model_info(folder_to, revision=branch, token=token)
     except RevisionNotFoundError:
         branch_exists = False
     finally:
         if branch_exists:
             print(b_info)
         else:
-            create_branch(model_to, branch=branch, token=token)    
-    upload_folder(folder_path=model_from, path_in_repo="", revision=branch, repo_id=model_to, commit_message=f"ckpt", token=token)
+            create_branch(folder_to, branch=branch, token=token)    
+    upload_folder(folder_path=folder_from, path_in_repo="", revision=branch, repo_id=folder_to, commit_message=f"folder", token=token)
     return "push folder done!"
-    
+ 
+def push_file(file_from, file_to, file_name, branch, token):
+    print(file_from)
+    try:
+        repo_exists = True
+        r_info = model_info(file_to, token=token)
+    except RepositoryNotFoundError:
+        repo_exists = False
+    finally:
+        if repo_exists:
+            print(r_info)
+        else:
+            create_repo(file_to, private=True, token=token)
+    try:
+        branch_exists = True
+        b_info = model_info(file_to, revision=branch, token=token)
+    except RevisionNotFoundError:
+        branch_exists = False
+    finally:
+        if branch_exists:
+            print(b_info)
+        else:
+            create_branch(file_to, branch=branch, token=token)    
+    upload_file(path_or_fileobj=file_from, path_in_repo=file_name, revision=branch, repo_id=file_to, commit_message=f"file", token=token)
+    return "push file done!"   
 
 def on_ui_tabs():     
     with gr.Blocks() as huggingface:
         gr.Markdown(
         """
         ### Push Folder to 🤗 Hugging Face
-        ckpt_model_from = 🖼 Windows: C:\\Users\\PC\\Desktop\\MyModelFolder 🐧 Linux: /home/user/app/stable-diffusion-webui/my-model-folder<br />
-        ckpt_model_to = camenduru/mymodel <br />
+        folder_from = 🖼 Windows: C:\\Users\\PC\\Desktop\\MyModelFolder 🐧 Linux: /home/user/app/stable-diffusion-webui/my-model-folder<br />
+        folder_to = camenduru/mymodel <br />
         branch = main <br />
         token = get from [https://huggingface.co/settings/tokens](https://huggingface.co/settings/tokens) new token role=write
         """)
         with gr.Group():
             with gr.Box():
                 with gr.Row().style(equal_height=True):
-                    text_ckpt_model_from = gr.Textbox(show_label=False, max_lines=1, placeholder="ckpt_model_from")
-                    text_ckpt_model_to = gr.Textbox(show_label=False, max_lines=1, placeholder="ckpt_model_to")
-                    text_ckpt_branch = gr.Textbox(show_label=False, value="main", max_lines=1, placeholder="ckpt_branch")
-                    text_ckpt_token = gr.Textbox(show_label=False, max_lines=1, placeholder="🤗 token")
-                    out_ckpt = gr.Textbox(show_label=False)
+                    text_folder_from = gr.Textbox(show_label=False, max_lines=1, placeholder="folder_from")
+                    text_folder_to = gr.Textbox(show_label=False, max_lines=1, placeholder="folder_to")
+                    text_folder_branch = gr.Textbox(show_label=False, value="main", max_lines=1, placeholder="branch")
+                    text_folder_token = gr.Textbox(show_label=False, max_lines=1, placeholder="🤗 token")
+                    out_folder = gr.Textbox(show_label=False)
                 with gr.Row().style(equal_height=True):
-                    btn_push_ckpt = gr.Button("Push Folder To 🤗")
-            btn_push_ckpt.click(push_ckpt, inputs=[text_ckpt_model_from, text_ckpt_model_to, text_ckpt_token, text_ckpt_branch], outputs=out_ckpt)
+                    btn_push_folder = gr.Button("Push Folder To 🤗")
+            btn_push_folder.click(push_folder, inputs=[text_folder_from, text_folder_to, text_folder_branch, text_folder_token], outputs=out_folder)
+        gr.Markdown(
+        """
+        ### Push File to 🤗 Hugging Face
+        file_from = 🖼 Windows: C:\\Users\\PC\\Desktop\\MyModelFolder\\model.ckpt 🐧 Linux: /home/user/app/stable-diffusion-webui/my-model-folder/model.ckpt<br />
+        file_to = camenduru/mymodel <br />
+        file_name = model.ckpt <br />
+        branch = main <br />
+        token = get from [https://huggingface.co/settings/tokens](https://huggingface.co/settings/tokens) new token role=write
+        """)
+        with gr.Group():
+            with gr.Box():
+                with gr.Row().style(equal_height=True):
+                    text_file_from = gr.Textbox(show_label=False, max_lines=1, placeholder="file_from")
+                    text_file_to = gr.Textbox(show_label=False, max_lines=1, placeholder="file_to")
+                    text_file_name = gr.Textbox(show_label=False, max_lines=1, placeholder="file_name")
+                    text_file_branch = gr.Textbox(show_label=False, value="main", max_lines=1, placeholder="branch")
+                    text_file_token = gr.Textbox(show_label=False, max_lines=1, placeholder="🤗 token")
+                    out_file = gr.Textbox(show_label=False)
+                with gr.Row().style(equal_height=True):
+                    btn_push_file = gr.Button("Push File To 🤗")
+            btn_push_file.click(push_file, inputs=[text_file_from, text_file_to, text_file_name, text_file_branch, text_file_token], outputs=out_file)
     return (huggingface, "Hugging Face", "huggingface"),
 script_callbacks.on_ui_tabs(on_ui_tabs)
