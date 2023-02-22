31541c4d9b2fa18fd3c7827a3937e2ea672456bf
camenduru <54370274+camenduru@users.noreply.github.com>
2023-02-22 14:19:40 +0300

    Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
 import launch
 
 if not launch.is_installed("huggingface_hub"):
-    launch.run_pip("install huggingface-hub==0.11.1", "requirements for Push to Hugging Face extension")
+    launch.run_pip("install huggingface-hub==0.11.0", "requirements for Push to Hugging Face extension")
